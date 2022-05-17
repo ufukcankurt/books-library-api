058be0cd154d365a9f61b0fe737c7d245e6115ab
ufukcankurt <ufuk.cnk@gmail.com>
2022-05-17 09:01:35 +0300

    Create | UPDATE User Function

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const verify = require("../verifyToken")
 const User = require("../models/User");
 const router = express.Router();
 
@@ -16,6 +17,35 @@ router.get("/:id", async (req, res) => {
         res.status(500).json(error)
     }
 })
+
+// Update User  
+router.put("/:id", verify, async (req, res) => {
+    // req.user.id === this is coming from verifyToken (req.user)
+    // allow if user id is equal or is admin
+    if (req.user.id === req.params.id || req.user.isAdmin) {
+        // first we have to check if the password has changed
+        if (req.body.password) {
+            const salt = await bcrypt.genSalt(10);
+            const hashedPassword = await bcrypt.hash(formData.password, salt);
+            req.body.password = hashedPassword
+        }
+
+        try {
+            // firstly we can find this user (req.params.id)
+            // and then we can set new properties  // we send all body 
+            const updatedUser = await User.findByIdAndUpdate(req.params.id, {
+                $set: req.body,
+            },{new:true}) // after setting new user
+            res.status(200).json(updatedUser)
+        } catch (error) {
+            res.status(500).json(error)
+        }
+
+    } else {
+        res.status(403).json("You can update only your account ") // 403|forbidden
+    }
+})
+
 
 
 module.exports = router;
