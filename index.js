596338d4cadd42e995154cf5348546ee5c34dd91
ufukcankurt <ufuk.cnk@gmail.com>
2022-05-17 07:44:14 +0300

    Create | necessary middewares && connections

--- index.js
+++ index.js
@@ -4,27 +4,29 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const helmet = require("helmet");
 const morgan = require("morgan");
+const cors = require("cors")
+
+const userRoute = require("./routes/users")
+const authRoute = require("./routes/auth")
 
 dotenv.config();
 
 mongoose.connect(
     process.env.MONGO_URL,
-    {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true,
-    },
+    { useNewUrlParser: true, useUnifiedTopology:true },
     () => {
         console.log("Connected to MONGODB");
-    }
-)
+    })
 
 //middleware
 app.use(express.json()); // body parser when you make  post request.. İt just gonna password it
 app.use(helmet());
 app.use(morgan("common")); // show on terminal| your date and request type and adress and status and duration of response
+app.use(cors())
 
-
+// address for rest API  || whenever go to this adress  it's gonna run this router.
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
 
 
 
