21bdf26acb7e8b8174b56907b003678ccb6f9cd4
ufukcankurt <ufuk.cnk@gmail.com>
2022-05-19 08:22:34 +0300

    Create | GET BOOK method

--- routes/books.js
+++ routes/books.js
@@ -53,4 +53,19 @@ router.delete("/:id", verify, async (req, res) => {
 })
 
 
+// GET
+router.get("/:id", verify, async (req, res) => {
+    // even if we're not an admin we can read book we can see any information about books. 
+    // So there is no need to be admin
+    try {
+        const movie = await Book.findById(req.params.id)
+        res.status(200).json(movie);
+    } catch (error) {
+        res.status(500).json(error)
+    }
+
+})
+
+
+
 module.exports = router;
