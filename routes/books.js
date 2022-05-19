46f7644a26a9ae56a7a8d3354b6e1cf1cb878745
ufukcankurt <ufuk.cnk@gmail.com>
2022-05-19 08:21:31 +0300

    Create | CREATE BOOK method

--- routes/books.js
+++ routes/books.js
@@ -4,6 +4,25 @@ const Book = require("../models/Book");
 const router = express.Router();
 
 
+// CREATE
+router.post("/", verify, async (req, res) => {
+    if (req.user.isAdmin) {
+        const newBook = new Book(req.body)
+
+        try {
+            const savedBook = await newBook.save();
+            res.status(201).json(savedBook);
+        } catch (error) {
+            res.status(500).json(error)
+        }
+    } else {
+        res.status(403).json("You're not allowed") // 403|forbidden
+    }
+})
+
+
+
+
 
 
 module.exports = router;
