38243dbfa2b8993d1150a4e7b64d8ce7d1f76211
ufukcankurt <ufuk.cnk@gmail.com>
2022-05-19 07:00:53 +0300

    Create | FOLLOW A USER method

--- routes/users.js
+++ routes/users.js
@@ -98,6 +98,27 @@ router.get("/friends/:userId", async (req, res)=> {
     }
 })
 
+// FOLLOW USER
+router.put("/:id/follow", async (req, res)=> {
+    if( req.body.userId !== req.params.id ){
+        try {
+            const user = await User.findById(req.params.id)
+            const currentUser = await User.findById(req.body.userId)
+            if(!user.followers.includes(req.body.userId)){
+                await user.updateOne({$push:{followers:req.body.userId}})
+                await currentUser.updateOne({$push:{followings:req.body.userId}})
+                res.status(200).json("User has been followed")
+            }else{
+                res.status(403).json("You already follow this user")
+            }
+        } catch (error) {
+            res.status(500).json(error)
+        }
+    }else{
+        res.status(403).json("You can't follow yourself")
+    }
+})
+
 // if we're not admin there is no sense to see all users.
 // GET ALL User
 router.get("/", verify, async (req, res) => { // "/?new=true" --> query
