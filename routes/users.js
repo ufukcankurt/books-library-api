3ecf774c82adc7922934ac44b7f7ea52e39b7c15
ufukcankurt <ufuk.cnk@gmail.com>
2022-05-23 21:07:02 +0300

    Update | User Route

--- routes/users.js
+++ routes/users.js
@@ -109,8 +109,8 @@ router.put("/:id/book", verify, async (req, res) => {
         try {
             const user = await User.findById(req.params.id);
             let sonuc = false;
-            user.bookShelf?.filter((item, i) => item.bookId === req.body.bookId ? sonuc = i : sonuc = "-1")
-            if (sonuc === "-1" || sonuc === false) {
+            user.bookShelf?.filter((item, i) => item.bookId === req.body.bookId ? sonuc = true : sonuc = false)
+            if (sonuc === false) {
                 await user.updateOne({ $push: { bookShelf: req.body } })
                 res.status(200).json("Book has been added");
             } else {
@@ -120,11 +120,9 @@ router.put("/:id/book", verify, async (req, res) => {
                 res.status(200).json(updatedBook)
             }
         } catch (error) {
-            console.log(error)
             res.status(500).json(error)
         }
     } else {
-        console.log("ELSE İÇİ You can update only your account ");
         res.status(403).json("You can update only your account ") // 403|forbidden
     }
 })
