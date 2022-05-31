3bb94107fb6591ab85e7ad966febf0827c87c228
ufukcankurt <ufuk.cnk@gmail.com>
2022-05-31 08:39:23 +0300

    Update | Auth function response

--- index.js
+++ index.js
@@ -43,5 +43,5 @@ app.use("/api/todayInHistory", todayInHistoryRoute);
 
 
 
-app.listen(process.env.PORT || 3000, () => console.log("Backend server is running!"))
+app.listen(process.env.PORT || 8000, () => console.log("Backend server is running!"))
 
--- routes/auth.js
+++ routes/auth.js
@@ -16,10 +16,10 @@ router.post("/register", async (req, res) => {
     const formData = req.body.formData;
 
     const userUsername = await User.findOne({ username: formData.username });
-    userUsername && res.status(401).json({ message: "Böyle bir kullanıcı ismi bulunmaktadır." });
+    // userUsername && res.status(401).json({ message: "Böyle bir kullanıcı ismi bulunmaktadır." });
 
     const userMail = await User.findOne({ email: formData.email });
-    userMail && res.status(401).json({ message: "Böyle bir E-mail adresi bulunmaktadır." });
+    // userMail && res.status(401).json({ message: "Böyle bir E-mail adresi bulunmaktadır." });
 
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(formData.password, salt);
@@ -27,21 +27,28 @@ router.post("/register", async (req, res) => {
     const generatedUserId = uuidv4();
     const sanitizedEmail = formData.email.toLowerCase()
     try {
-        const newUser = new User({
-            userId: generatedUserId,
-            fullname: formData.full_name,
-            username: formData.username,
-            email: sanitizedEmail,
-            password: hashedPassword,
-            dob_day: formData.dob_day,
-            dob_month: formData.dob_month,
-            dob_year: formData.dob_year,
-            gender: formData.gender_identity
-        })
+        if (userUsername) {
+            res.status(401).json({ message: "Böyle bir kullanıcı ismi bulunmaktadır." });
+        } else if (userMail) {
+            res.status(401).json({ message: "Böyle bir E-mail adresi bulunmaktadır." });
+        } else {
+            const newUser = new User({
+                userId: generatedUserId,
+                fullname: formData.full_name,
+                username: formData.username,
+                email: sanitizedEmail,
+                password: hashedPassword,
+                dob_day: formData.dob_day,
+                dob_month: formData.dob_month,
+                dob_year: formData.dob_year,
+                gender: formData.gender_identity
+            })
 
-        // mongoose automatically will save your user to DB
-        const user = await newUser.save();
-        res.status(200).json(user);
+            // mongoose automatically will save your user to DB
+            const user = await newUser.save();
+            res.status(200).json(user);
+        }
+
     } catch (error) {
         res.status(500).json(error)
     }
@@ -52,11 +59,11 @@ router.post("/register", async (req, res) => {
 router.post("/login", async (req, res) => {
     try {
         const user = await User.findOne({ username: req.body.username });
-        !user && res.status(401).json("Yanlış kullanıcı adı!")
+        // !user && res.status(401).json("Yanlış kullanıcı adı!")
 
         // compare passwords
         const validPassword = await bcrypt.compare(req.body.password, user.password)
-        !validPassword && res.status(400).json("Yanlış şifre!"); // 400 - wrong password
+        // !validPassword && res.status(400).json("Yanlış şifre!"); // 400 - wrong password
 
         // _doc == all the information we receive
         const { password, ...info } = user._doc;
@@ -64,7 +71,15 @@ router.post("/login", async (req, res) => {
         // CREATE JWT
         const accessToken = jwt.sign({ id: user._id, isAdmin: user.isAdmin }, process.env.SECRET_KEY, { expiresIn: "365d" })
 
-        res.status(200).json({ ...info, accessToken })
+        if (!user) {
+            res.status(401).json("Yanlış kullanıcı adı!")
+        } else if (!validPassword) {
+            res.status(400).json("Yanlış şifre!")
+        } else {
+            res.status(200).json({ ...info, accessToken })
+        }
+
+
     } catch (error) {
         res.status(500).json(error)
     }
