46744f8125ce7c50c0dc377fea06a9dc35dd2d36
ufukcankurt <ufuk.cnk@gmail.com>
2022-07-28 12:20:25 +0300

    Add | Add book to author Method

--- routes/authors.js
+++ routes/authors.js
@@ -66,7 +66,7 @@ router.delete("/:id", verify, async (req, res) => {
 
 
 // GET AUTHORS BY Author Name
-router.get("/get/author/name", async (req, res) => {
+router.get("/get/author/name", verify, async (req, res) => {
     const { q } = req.query
     try {
         const author = await Author.find({ $or: [{ authorName: { $regex: q, $options: 'i' } }] })
@@ -75,6 +75,25 @@ router.get("/get/author/name", async (req, res) => {
     } catch (error) {
         res.status(500).json(error)
     }
+})
+
+// ADD BOOK TO AUTHOR
+router.put("/add/book/:id", verify, async (req, res) => {
+
+    if (req.user.isAdmin) {
+        try {
+            const author = await Author.findById(req.params.id)
+
+            await author.updateOne({ $push: { authorBooks: req.body } })
+            res.status(201).json("Book has been added to Author");
+
+        } catch (error) {
+            res.status(500).json(error)
+        }
+    } else {
+        res.status(403).json("You cannot do this. Not Allowed!")
+    }
+
 })
 
 
